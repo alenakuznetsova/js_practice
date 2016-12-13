c970e5cc20e58544749ce96f02cca895ac6d3459
alena kuznetsova <alena.klevtsova@gmail.com>
2016-12-13 23:16:52 +0200

    create folder with practice array and strings and remowe there all folders

--- confirmEnding/confirmEnding.js
+++
@@ -1,9 +0,0 @@
-function confirmEnding(str, target) {
-  var patternLength = target.length;
-  var newString = str.substr(str.length-patternLength, patternLength);
-  
-  if (target == newString){
-    return true;
-  } else return false;
-  
-}
--- factorial/factorial.js
+++
@@ -1,9 +0,0 @@
-function factorialize(num) {
-  var fac = 1;
-    for  (var i = 1; i<=num; i++){    
-      fac = fac*i;    
-    }
-    return fac;    
-}
-
-factorialize(5);
